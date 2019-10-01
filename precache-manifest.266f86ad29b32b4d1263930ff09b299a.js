7a6bc38abb0d4abb0666f377a135a4f2ffbeab5f
David Codina <davidmcodina@hotmail.com>
2019-10-01 15:09:25 -0600

    Updates

---
+++ precache-manifest.266f86ad29b32b4d1263930ff09b299a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afb5df3c6fc685f290162c354c1736ed",
+    "url": "/react-contact-manager/index.html"
+  },
+  {
+    "revision": "35edb41e6795aa76a90e",
+    "url": "/react-contact-manager/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "9521ad7764c49fd6fa88",
+    "url": "/react-contact-manager/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "35edb41e6795aa76a90e",
+    "url": "/react-contact-manager/static/js/2.fc068368.chunk.js"
+  },
+  {
+    "revision": "9521ad7764c49fd6fa88",
+    "url": "/react-contact-manager/static/js/main.6d72c8df.chunk.js"
+  },
+  {
+    "revision": "ddd1cb143a96bb9a3649",
+    "url": "/react-contact-manager/static/js/runtime-main.89aa322f.js"
+  }
+]);
